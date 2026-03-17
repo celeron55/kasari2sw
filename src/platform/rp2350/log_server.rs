879dacbbcfff54f531f6db1df0be4098872d125c
Perttu Ahola <celeron55@gmail.com>
2026-03-17 19:22:23 +0200

    RP2350: Stream logs to TCP clients
    Read from ring buffer and send to connected TCP clients on port 8082.

--- src/platform/rp2350/log_server.rs
+++ src/platform/rp2350/log_server.rs
@@ -1,7 +1,9 @@
+use crate::logging::get_ring_buffer;
 use embassy_net::tcp::TcpSocket;
 use embassy_net::Stack;
 use embassy_time::Timer;
 use log::info;
+use ringbuffer::RingBuffer;
 
 const TCP_PORT: u16 = 8082;
 const RX_BUFFER_SIZE: usize = 4096;
@@ -9,7 +11,7 @@ const TX_BUFFER_SIZE: usize = 4096;
 
 #[embassy_executor::task]
 pub async fn log_server_task(stack: Stack<'static>) -> ! {
-    info!("TCP server starting on port {}", TCP_PORT);
+    info!("Log TCP server starting on port {}", TCP_PORT);
 
     let mut rx_buffer = [0; RX_BUFFER_SIZE];
     let mut tx_buffer = [0; TX_BUFFER_SIZE];
@@ -29,10 +31,35 @@ pub async fn log_server_task(stack: Stack<'static>) -> ! {
 
         info!("TCP client connected from {:?}", socket.remote_endpoint());
 
-        // Just wait for disconnect
+        // Stream logs to client
         loop {
-            Timer::after_millis(1000).await;
+            // Read from ring buffer
+            let ring_ref = get_ring_buffer();
+            let data = cortex_m::interrupt::free(|cs| {
+                let mut rb = ring_ref.borrow(cs);
+                let ring = rb.borrow_mut();
+                let len = ring.len();
+                let mut vec = heapless::Vec::<u8, 4096>::new();
+                for b in ring.iter().take(len) {
+                    let _ = vec.push(*b);
+                }
+                vec
+            });
+
+            if !data.is_empty() {
+                match socket.write(&data).await {
+                    Ok(_) => {}
+                    Err(e) => {
+                        info!("Write error: {:?}", e);
+                        break;
+                    }
+                }
+            }
+
+            Timer::after_millis(50).await;
         }
+
+        info!("TCP client disconnected");
     }
 }
 
